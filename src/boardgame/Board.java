688a96fd1bf37f915d7147d2ec72f9f94c92b75c
Felipe Henrique <felipeh.soares3@gmail.com>
2020-04-08 12:06:41 -0700

    Placing pieces on the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -37,4 +37,10 @@ public class Board {
 		return pieces[position.getRow()][position.getColumn()];
 	}
 	
+	public void placePiece(Piece piece, Position position) {
+		pieces[position.getRow()][position.getColumn()] = piece;
+		//POSITION PASSA A NÃO SER MAIS NULL
+		piece.position = position;
+	}
+	
 }
